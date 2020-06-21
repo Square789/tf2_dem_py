f53a8617cf2d55b8ab822d5c536a342b995553fd
Square789 <46634729+Square789@users.noreply.github.com>
2020-06-21 03:07:11 +0200

    ohh man pls help am not good with git

--- run_copy.py
+++ run_copy.py
@@ -1,9 +1,5 @@
 
-<<<<<<< Updated upstream
-# Shoddy run script
-=======
 # Shoddy directory-wise run script
->>>>>>> Stashed changes
 
 import os
 import json
@@ -20,20 +16,12 @@ print(p)
 for i, file in enumerate([os.path.join(p, i) for i in os.listdir(p) if i.endswith(".dem")]):
 	print(file)
 	dp = DemoParser(
-<<<<<<< Updated upstream
-		bytes(file, encoding = "utf-8"),
-=======
->>>>>>> Stashed changes
 		(
 			FLAGS.CHAT |
 			FLAGS.GAME_EVENTS
 		)
 	)
-<<<<<<< Updated upstream
-	res = dp.parse()
-=======
 	res = dp.parse(file)
->>>>>>> Stashed changes
 	# pprint(res, sort_dicts = False, compact = True)
 
 	with open(f"lastdemo{i}.json", "w") as h:
